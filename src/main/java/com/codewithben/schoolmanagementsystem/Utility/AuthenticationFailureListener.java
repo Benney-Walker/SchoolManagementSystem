f5a46f6aeb51e4bf585dbc39ab2320ac74d6eb58
Benney-Walker <benjaminbadubenney@gmail.com>
2026-04-10 13:14:14 +0000

    Added authentication failure listener for logging

---
+++ src/main/java/com/codewithben/schoolmanagementsystem/Utility/AuthenticationFailureListener.java
@@ -0,0 +1,28 @@
+package com.codewithben.schoolmanagementsystem.Utility;
+
+import com.codewithben.schoolmanagementsystem.Service.LoggingService;
+import org.springframework.context.ApplicationListener;
+import org.springframework.security.authentication.event.AbstractAuthenticationFailureEvent;
+import org.springframework.stereotype.Component;
+
+@Component
+public class AuthenticationFailureListener implements ApplicationListener<AbstractAuthenticationFailureEvent> {
+
+    private final LoggingService loggingService;
+
+    public AuthenticationFailureListener(LoggingService loggingService) {
+        this.loggingService = loggingService;
+    }
+
+    @Override
+    public void onApplicationEvent(AbstractAuthenticationFailureEvent event) {
+        String username = (String) event.getAuthentication().getPrincipal();
+
+        loggingService.logActivity(
+                "AUTHENTICATION",
+                "N/A",
+                username,
+                "FAILED"
+        );
+    }
+}
