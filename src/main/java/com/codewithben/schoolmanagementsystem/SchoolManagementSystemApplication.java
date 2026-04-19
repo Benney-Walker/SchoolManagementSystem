a3406c1af2afffa35690024f16432e507eb42964
Benney-Walker <benjaminbadubenney@gmail.com>
2026-04-19 15:29:55 +0000

    Added env file for local database configuration

--- src/main/java/com/codewithben/schoolmanagementsystem/SchoolManagementSystemApplication.java
+++ src/main/java/com/codewithben/schoolmanagementsystem/SchoolManagementSystemApplication.java
@@ -1,12 +1,22 @@
 package com.codewithben.schoolmanagementsystem;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
+@EnableWebSecurity
 @SpringBootApplication
 public class SchoolManagementSystemApplication {
 
     public static void main(String[] args) {
+
+        Dotenv dotenv = Dotenv.load();
+
+        dotenv.entries().forEach(entry -> {
+            System.setProperty(entry.getKey(), entry.getValue());
+        });
+
         SpringApplication.run(SchoolManagementSystemApplication.class, args);
     }
 
