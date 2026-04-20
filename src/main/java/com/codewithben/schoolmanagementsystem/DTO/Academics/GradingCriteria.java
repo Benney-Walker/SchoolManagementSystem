de6b90e9b4e4297865680e3559940cc1423c73e1
Benney-Walker <benjaminbadubenney@gmail.com>
2026-04-20 03:03:33 +0000

    Removed staff Id from grading criteria

--- src/main/java/com/codewithben/schoolmanagementsystem/DTO/Academics/GradingCriteria.java
+++ src/main/java/com/codewithben/schoolmanagementsystem/DTO/Academics/GradingCriteria.java
@@ -9,54 +9,26 @@ public class GradingCriteria {
 
     private String gradeDescription;
 
-    private String staffId;
-
-    public GradingCriteria(Double lowerRange, Double upperRange, String grade, String gradeDescription,
-                           String staffId) {
+    public GradingCriteria(Double lowerRange, Double upperRange, String grade, String gradeDescription) {
         this.lowerRange = lowerRange;
         this.upperRange = upperRange;
         this.grade = grade;
         this.gradeDescription = gradeDescription;
-        this.staffId = staffId;
     }
 
     public Double getLowerRange() {
         return lowerRange;
     }
 
-    public void setLowerRange(Double lowerRange) {
-        this.lowerRange = lowerRange;
-    }
-
     public Double getUpperRange() {
         return upperRange;
-    }
-
-    public void setUpperRange(Double upperRange) {
-        this.upperRange = upperRange;
     }
 
     public String getGrade() {
         return grade;
     }
 
-    public void setGrade(String grade) {
-        this.grade = grade;
-    }
-
     public String getGradeDescription() {
         return gradeDescription;
-    }
-
-    public void setGradeDescription(String gradeDescription) {
-        this.gradeDescription = gradeDescription;
-    }
-
-    public String getStaffId() {
-        return staffId;
-    }
-
-    public void setStaffId(String staffId) {
-        this.staffId = staffId;
     }
 }
