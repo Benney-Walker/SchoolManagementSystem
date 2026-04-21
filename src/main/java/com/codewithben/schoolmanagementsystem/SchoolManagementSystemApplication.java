519fbf3a4463779fa2fc541a3fdc17d6b5b1d88f
Benney-Walker <benjaminbadubenney@gmail.com>
2026-04-21 05:16:21 +0000

    Solving crashing

--- src/main/java/com/codewithben/schoolmanagementsystem/SchoolManagementSystemApplication.java
+++ src/main/java/com/codewithben/schoolmanagementsystem/SchoolManagementSystemApplication.java
@@ -11,7 +11,9 @@ public class SchoolManagementSystemApplication {
 
     public static void main(String[] args) {
 
-        Dotenv dotenv = Dotenv.load();
+        Dotenv dotenv = Dotenv.configure()
+                .ignoreIfMissing()
+                .load();
 
         dotenv.entries().forEach(entry -> {
             System.setProperty(entry.getKey(), entry.getValue());
