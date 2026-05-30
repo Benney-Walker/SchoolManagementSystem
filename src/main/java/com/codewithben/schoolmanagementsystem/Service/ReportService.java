5b6decaa1ed8b9c479667d17cc7e95db832a3c1e
Benney-Walker <benjaminbadubenney@gmail.com>
2026-05-30 14:37:21 +0000

    Added placeholder for conduct to replace null

--- src/main/java/com/codewithben/schoolmanagementsystem/Service/ReportService.java
+++ src/main/java/com/codewithben/schoolmanagementsystem/Service/ReportService.java
@@ -92,8 +92,7 @@ public class ReportService {
             String averageScore = result.getAverageScore().toString();
             String position = result.getPosition();
             String totalStudents = String.valueOf(studentResults.size());
-            String semesterRemark = result.getConduct().getClassTeacherRemark() == null ?
-                    "Not field yet" : result.getConduct().getClassTeacherRemark();
+            String semesterRemark = "Not field yet";
 
             List<ViewStudentsSubjectsResults> scoresList = new ArrayList<>();
 
