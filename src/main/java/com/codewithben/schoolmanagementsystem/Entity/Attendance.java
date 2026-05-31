d4d2841f7c76d09ab56955457a19e36de5a20015
Benney-Walker <benjaminbadubenney@gmail.com>
2026-05-31 14:30:13 +0000

    Cleared boiler plate to use lombok

--- src/main/java/com/codewithben/schoolmanagementsystem/Entity/Attendance.java
+++ src/main/java/com/codewithben/schoolmanagementsystem/Entity/Attendance.java
@@ -2,9 +2,15 @@ package com.codewithben.schoolmanagementsystem.Entity;
 
 import com.codewithben.schoolmanagementsystem.Contants.AttendanceStatus;
 import jakarta.persistence.*;
+import lombok.*;
 
 import java.time.LocalDate;
 
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 @Entity
 public class Attendance {
     @Id
@@ -27,60 +33,4 @@ public class Attendance {
 
     @ManyToOne
     private Staffs markedBy;
-
-    public long getId() {
-        return id;
-    }
-
-    public void setId(long id) {
-        this.id = id;
-    }
-
-    public Students getStudent() {
-        return student;
-    }
-
-    public void setStudent(Students student) {
-        this.student = student;
-    }
-
-    public Level getLevel() {
-        return level;
-    }
-
-    public void setLevel(Level level) {
-        this.level = level;
-    }
-
-    public Semester getSemester() {
-        return semester;
-    }
-
-    public void setSemester(Semester semester) {
-        this.semester = semester;
-    }
-
-    public LocalDate getDateMarked() {
-        return dateMarked;
-    }
-
-    public void setDateMarked(LocalDate date) {
-        this.dateMarked = date;
-    }
-
-    public AttendanceStatus getStatus() {
-        return status;
-    }
-
-    public void setStatus(AttendanceStatus status) {
-        this.status = status;
-    }
-
-    public Staffs getMarkedBy() {
-        return markedBy;
-    }
-
-    public void setMarkedBy(Staffs markedBy) {
-        this.markedBy = markedBy;
-    }
 }
--- src/main/java/com/codewithben/schoolmanagementsystem/Entity/Semester.java
+++ src/main/java/com/codewithben/schoolmanagementsystem/Entity/Semester.java
@@ -1,11 +1,17 @@
 package com.codewithben.schoolmanagementsystem.Entity;
 
 import jakarta.persistence.*;
+import lombok.*;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.List;
 
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 @Entity
 public class Semester {
 
@@ -23,61 +29,4 @@ public class Semester {
     @ManyToOne
     @JoinColumn(name = "Institution_institutionId")
     private Institution institution;
-
-    public String getSemesterID() {
-        return semesterID;
-    }
-
-    public void setSemesterID(String semesterID) {
-
-        this.semesterID = semesterID;
-    }
-
-    public String getSemesterName() {
-        return semesterName;
-    }
-
-    public void setSemesterName(String semesterName) {
-        this.semesterName = semesterName;
-    }
-
-    public LocalDate getSemesterStartDate() {
-
-        return semesterStartDate;
-    }
-
-    public void setSemesterStartDate(LocalDate semesterStartDate) {
-
-        this.semesterStartDate = semesterStartDate;
-    }
-
-    public LocalDate getSemesterEndDate() {
-
-        return semesterEndDate;
-    }
-
-    public void setSemesterEndDate(LocalDate semesterEndDate) {
-
-        this.semesterEndDate = semesterEndDate;
-    }
-
-    public String getAcademicYear() {
-
-        return academicYear;
-    }
-
-    public void setAcademicYear(String academicYear) {
-
-        this.academicYear = academicYear;
-    }
-
-    public Institution getInstitution() {
-
-        return institution;
-    }
-
-    public void setInstitution(Institution institution) {
-
-        this.institution = institution;
-    }
 }
