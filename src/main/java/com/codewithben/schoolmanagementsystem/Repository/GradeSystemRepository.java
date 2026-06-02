043ef5eef5b3dcf0e0e3b67025489342bf727169
Benney-Walker <benjaminbadubenney@gmail.com>
2026-06-02 16:38:27 +0000

    Added two queries

--- src/main/java/com/codewithben/schoolmanagementsystem/Repository/GradeSystemRepository.java
+++ src/main/java/com/codewithben/schoolmanagementsystem/Repository/GradeSystemRepository.java
@@ -5,8 +5,14 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface GradeSystemRepository extends JpaRepository<GradeSystem, Long> {
+
     List<GradeSystem> findAllByInstitution_InstitutionId(String institutionId);
+
+    Optional<GradeSystem> findByGradeAndInstitution_InstitutionId(String grade, String institutionId);
+
+    Optional<GradeSystem> findById(int id);
 }
