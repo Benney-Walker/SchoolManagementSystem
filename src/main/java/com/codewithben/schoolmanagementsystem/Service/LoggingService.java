b0b5b027cfc49b780d968bceb0b66bbc281ad2f8
Benney-Walker <benjaminbadubenney@gmail.com>
2026-06-03 00:23:54 +0000

    Added method to retrieve recent logs

--- src/main/java/com/codewithben/schoolmanagementsystem/Service/LoggingService.java
+++ src/main/java/com/codewithben/schoolmanagementsystem/Service/LoggingService.java
@@ -2,7 +2,9 @@ package com.codewithben.schoolmanagementsystem.Service;
 
 import com.codewithben.schoolmanagementsystem.Contants.LogStatus;
 import com.codewithben.schoolmanagementsystem.Contants.LogType;
+import com.codewithben.schoolmanagementsystem.DTO.Logs.LogsDTO;
 import com.codewithben.schoolmanagementsystem.Entity.Logs;
+import com.codewithben.schoolmanagementsystem.Entity.Staffs;
 import com.codewithben.schoolmanagementsystem.Repository.LogsRepository;
 import com.codewithben.schoolmanagementsystem.Repository.StaffsRepository;
 import com.codewithben.schoolmanagementsystem.Utility.UtilityClass;
@@ -13,6 +15,9 @@ import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
 import java.time.LocalTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 
 @AllArgsConstructor
 @Service
@@ -37,5 +42,49 @@ public class LoggingService {
         } catch (IllegalArgumentException e) {
             throw new RuntimeException(e);
         }
+    }
+
+    public ResponseEntity<?> getRecentActivity(String staffId) {
+
+        Staffs staff = staffService.getStaffDetails(staffId);
+        if (staff == null) {
+            logActivity(LogType.LOGS, "N/A", staffId, "FAILED");
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(Map.of(
+                    "message", "Invalid staff Id"
+            ));
+        }
+
+        List<Logs> recentLogs = logsRepository.findByInstitution_InstitutionId(staff.getInstitution().getInstitutionId());
+        if (recentLogs == null || recentLogs.isEmpty()) {
+            logActivity(LogType.LOGS, "N/A", staffId, "FAILED");
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(Map.of(
+                    "message", "No recent logs found"
+            ));
+        }
+
+        List<LogsDTO> logList = new ArrayList<>();
+        int count = 0;
+        //Retrieve logs
+        for (Logs log : recentLogs) {
+            if (count >= 10) {
+                break;
+            }
+
+            LogsDTO logsDTO = LogsDTO.builder()
+                    .id(log.getActionId())
+                    .time(log.getActionTime().toString())
+                    .date(log.getActionDate().toString())
+                    .type(log.getActionType().name())
+                    .message(log.getActionData())
+                    .status(log.getStatus().name())
+                    .createdBy(log.getCreatedBy().getFirstName() +
+                            " " + log.getCreatedBy().getLastName())
+                    .build();
+
+            logList.add(logsDTO);
+            count++;
+        }
+
+        return ResponseEntity.ok(logList);
     }
 }
