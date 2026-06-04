3af1872458c3cf21742579b436266458c2369034
Benney-Walker <benjaminbadubenney@gmail.com>
2026-06-04 21:17:15 +0000

    Refactored constants package name

--- src/main/java/com/codewithben/schoolmanagementsystem/Repository/AttendanceRepository.java
+++ src/main/java/com/codewithben/schoolmanagementsystem/Repository/AttendanceRepository.java
@@ -1,6 +1,6 @@
 package com.codewithben.schoolmanagementsystem.Repository;
 
-import com.codewithben.schoolmanagementsystem.Contants.AttendanceStatus;
+import com.codewithben.schoolmanagementsystem.Constants.AttendanceStatus;
 import com.codewithben.schoolmanagementsystem.Entity.Attendance;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
