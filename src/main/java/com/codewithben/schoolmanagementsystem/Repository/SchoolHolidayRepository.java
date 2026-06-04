d9cd87d0c5b7a9b6944113a41a0db28605ceeaff
Benney-Walker <benjaminbadubenney@gmail.com>
2026-06-04 13:32:05 +0000

    Added 3 queries

--- src/main/java/com/codewithben/schoolmanagementsystem/Repository/SchoolHolidayRepository.java
+++ src/main/java/com/codewithben/schoolmanagementsystem/Repository/SchoolHolidayRepository.java
@@ -1,9 +1,19 @@
 package com.codewithben.schoolmanagementsystem.Repository;
 
+import com.codewithben.schoolmanagementsystem.Contants.HolidayType;
 import com.codewithben.schoolmanagementsystem.Entity.SchoolHoliday;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 public interface SchoolHolidayRepository extends JpaRepository<SchoolHoliday, Integer> {
+
+    Optional<SchoolHoliday> findByHolidayId(int holidayId);
+
+    Optional<SchoolHoliday> findByHolidayNameAndInstitution_InstitutionId(HolidayType holidayName, String institutionId);
+
+    List<SchoolHoliday> findBySemester_SemesterID(String semesterId);
 }
