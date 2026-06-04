e9921bbe0423bf5f1b5f3157359d303e961a9dc9
Benney-Walker <benjaminbadubenney@gmail.com>
2026-06-04 21:11:09 +0000

    Added log action enum types

---
+++ src/main/java/com/codewithben/schoolmanagementsystem/Constants/LogAction.java
@@ -0,0 +1,12 @@
+package com.codewithben.schoolmanagementsystem.Constants;
+
+public enum LogAction {
+    CREATE,
+    READ,
+    UPDATE,
+    DELETE,
+    PROMOTE,
+    LOGIN,
+    RESET,
+    SUBSCRIPTION
+}
