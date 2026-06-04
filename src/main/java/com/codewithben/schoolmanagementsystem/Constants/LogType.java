7d9e0231476243eb833d71ef926a186093873ca5
Benney-Walker <benjaminbadubenney@gmail.com>
2026-06-04 21:10:22 +0000

    Updated log types

---
+++ src/main/java/com/codewithben/schoolmanagementsystem/Constants/LogType.java
@@ -0,0 +1,19 @@
+package com.codewithben.schoolmanagementsystem.Contants;
+
+public enum LogType {
+    ATTENDANCE,
+    CONDUCT,
+    FEES,
+    FEES_REPORT,
+    GRADE,
+    INSTITUTION,
+    CLASS,
+    LOG,
+    RESULT,
+    SCHOOL_HOLIDAY,
+    TERM,
+    STAFF,
+    STUDENT,
+    SUBJECT,
+    SUBJECT_SCORE
+}
