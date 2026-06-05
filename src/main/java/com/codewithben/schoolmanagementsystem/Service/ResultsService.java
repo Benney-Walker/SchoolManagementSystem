cf539a30650452b5d65909d82c105ba2768e08b2
Benney-Walker <benjaminbadubenney@gmail.com>
2026-06-05 17:38:22 +0000

    Initialized ResultsService

---
+++ src/main/java/com/codewithben/schoolmanagementsystem/Service/ResultsService.java
@@ -0,0 +1,11 @@
+package com.codewithben.schoolmanagementsystem.Service;
+
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@AllArgsConstructor
+@Service
+public class ResultsService {
+
+
+}
