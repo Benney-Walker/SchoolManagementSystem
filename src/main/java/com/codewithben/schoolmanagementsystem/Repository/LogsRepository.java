8c38b7a42a09e328b2232c099cf8110ed2c06a44
Benney-Walker <benjaminbadubenney@gmail.com>
2026-06-05 12:48:28 +0000

    Added database queries

--- src/main/java/com/codewithben/schoolmanagementsystem/Repository/LogsRepository.java
+++ src/main/java/com/codewithben/schoolmanagementsystem/Repository/LogsRepository.java
@@ -10,5 +10,15 @@ import java.util.List;
 @Repository
 public interface LogsRepository extends JpaRepository<Logs, Long> {
 
-    List<Logs> findByInstitution_InstitutionIdAndActionDateOrderByActionIdDesc(String institutionId, LocalDate date);
+    List<Logs> findByInstitution_InstitutionIdAndActionDateOrderByActionIdDesc(
+            String institutionId, LocalDate date
+    );
+
+    List<Logs> findByStaff_StaffIdAndActionDateBetweenOrderByActionIdDesc(
+            String staffId, LocalDate startDate, LocalDate endDate
+    );
+
+    List<Logs> findByInstitution_InstitutionIdAndActionDateBetweenOrderByActionIdDesc(
+            String institutionId, LocalDate startDate, LocalDate endDate
+    );
 }
