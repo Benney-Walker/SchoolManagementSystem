a7bd5ecb77087284645bf3aeec13e4a21d561074
Benney-Walker <benjaminbadubenney@gmail.com>
2026-06-07 20:38:36 +0000

    Project file

--- src/main/java/com/codewithben/schoolmanagementsystem/CompileReport.java
+++ src/main/java/com/codewithben/schoolmanagementsystem/CompileReport.java
@@ -5,8 +5,8 @@ import net.sf.jasperreports.engine.JasperCompileManager;
 public class CompileReport {
     public static void main(String[] args) throws Exception {
         JasperCompileManager.compileReportToFile(
-                "src/main/resources/reports/sba_report.jrxml",
-                "src/main/resources/reports/sba_report.jasper"
+                "src/main/resources/reports/Daffodils/student_report_card.jrxml",
+                "src/main/resources/reports/Daffodils/student_report_card.jasper"
         );
         System.out.println("Done! .jasper file created.");
     }
