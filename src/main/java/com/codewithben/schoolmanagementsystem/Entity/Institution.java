5903b683cdec9dd30e06d472de7ae1325236bf10
Benney-Walker <benjaminbadubenney@gmail.com>
2026-06-13 23:40:50 +0000

    Fixed boiler plate

--- src/main/java/com/codewithben/schoolmanagementsystem/Entity/Institution.java
+++ src/main/java/com/codewithben/schoolmanagementsystem/Entity/Institution.java
@@ -1,9 +1,15 @@
 package com.codewithben.schoolmanagementsystem.Entity;
 
 import jakarta.persistence.*;
+import lombok.*;
 
 import java.util.List;
 
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 @Entity
 public class Institution {
 
@@ -29,69 +35,5 @@ public class Institution {
     private List<Fees> fees;
 
     @OneToMany(mappedBy = "institution")
-    private List<FeesReport> feesReport;
-
-    public String getInstitutionId() {
-        return institutionId;
-    }
-
-    public void setInstitutionId(String institutionId) {
-        this.institutionId = institutionId;
-    }
-
-    public String getInstitutionName() {
-        return institutionName;
-    }
-
-    public void setInstitutionName(String institutionName) {
-        this.institutionName = institutionName;
-    }
-
-    public List<Staffs> getStaff() {
-        return staff;
-    }
-
-    public void setStaff(List<Staffs> staff) {
-        this.staff = staff;
-    }
-
-    public List<Students> getStudents() {
-        return students;
-    }
-
-    public void setStudents(List<Students> students) {
-        this.students = students;
-    }
-
-    public List<Level> getLevels() {
-        return level;
-    }
-
-    public void setLevel(List<Level> level) {
-        this.level = level;
-    }
-
-    public List<Semester> getSemester() {
-        return semester;
-    }
-
-    public void setSemester(List<Semester> semester) {
-        this.semester = semester;
-    }
-
-    public List<Fees> getFees() {
-        return fees;
-    }
-
-    public void setFees(List<Fees> fees) {
-        this.fees = fees;
-    }
-
-    public List<FeesReport> getFeesReport() {
-        return feesReport;
-    }
-
-    public void setFeesReport(List<FeesReport> feesReport) {
-        this.feesReport = feesReport;
-    }
+    private List<PaymentRecords> paymentRecords;
 }
