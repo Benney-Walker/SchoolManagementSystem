a4f8b6a215309d68891ec0277e422bba53e33f46
Benney-Walker <benjaminbadubenney@gmail.com>
2026-06-13 23:30:28 +0000

    Cleared boiler plate

--- src/main/java/com/codewithben/schoolmanagementsystem/Entity/Fees.java
+++ src/main/java/com/codewithben/schoolmanagementsystem/Entity/Fees.java
@@ -1,9 +1,15 @@
 package com.codewithben.schoolmanagementsystem.Entity;
 
 import jakarta.persistence.*;
+import lombok.*;
 
 import java.util.List;
 
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 @Entity
 public class Fees {
 
@@ -28,58 +34,4 @@ public class Fees {
     @ManyToOne
     @JoinColumn(name = "institution_institutionId")
     private Institution institution;
-
-    public int getFeesId() {
-
-        return feesId;
-    }
-
-    public void setFeesId(int feesId) {
-
-        this.feesId = feesId;
-    }
-
-    public Double getAmountToBePayed() {
-
-        return amountToBePayed;
-    }
-
-    public void setAmountToBePayed(Double amountToBePayed) {
-
-        this.amountToBePayed = amountToBePayed;
-    }
-
-    public List<FeesReport> getFeesReport() {
-        return feesReport;
-    }
-
-    public void setFeesReport(List<FeesReport> feesReport) {
-        this.feesReport = feesReport;
-    }
-
-    public Semester getSemester() {
-
-        return semester;
-    }
-
-    public void setSemester(Semester semester) {
-
-        this.semester = semester;
-    }
-
-    public Level getLevel() {
-        return level;
-    }
-
-    public void setLevel(Level level) {
-        this.level = level;
-    }
-
-    public Institution getInstitution() {
-        return institution;
-    }
-
-    public void setInstitution(Institution institution) {
-        this.institution = institution;
-    }
 }
