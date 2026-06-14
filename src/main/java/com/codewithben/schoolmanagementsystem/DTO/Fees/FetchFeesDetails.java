79205a9b42f9ffba220ae5a9b8ab605857210bd0
Benney-Walker <benjaminbadubenney@gmail.com>
2026-06-14 10:42:07 +0000

    Added fee Id field to dto

--- src/main/java/com/codewithben/schoolmanagementsystem/DTO/Fees/FetchFeesDetails.java
+++ src/main/java/com/codewithben/schoolmanagementsystem/DTO/Fees/FetchFeesDetails.java
@@ -10,6 +10,8 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class FetchFeesDetails {
+    private int feesId;
+
     private String amount;
 
     private String semesterId;
