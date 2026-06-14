3056ddec415080366828f13d4dca6dadf587553d
Benney-Walker <benjaminbadubenney@gmail.com>
2026-06-14 10:42:49 +0000

    Fixed mapping errors and added nullable constraints

--- src/main/java/com/codewithben/schoolmanagementsystem/Entity/PaymentRecords.java
+++ src/main/java/com/codewithben/schoolmanagementsystem/Entity/PaymentRecords.java
@@ -15,23 +15,29 @@ public class PaymentRecords {
     @Id
     private String recordsId;
 
+    @Column(nullable = false)
     private double amountPaid;
 
+    @Column(nullable = false)
     private double feesBalance;
 
+    @Column(nullable = false)
     private String personWhoPaid;
 
+    @Column(nullable = false)
     private String phoneNumber;
 
+    @Column(nullable = false)
     private LocalDate dateOfPayment;
 
+    @Column(nullable = false)
     private boolean isDeleted = false;
 
     @ManyToOne
-    @JoinColumn(name = "fee_record_recordId")
+    @JoinColumn(name = "feeRecord_recordId", nullable = false)
     private StudentFeeRecord feeRecord;
 
     @ManyToOne
-    @JoinColumn(name = "institution_institutionId")
+    @JoinColumn(name = "institution_institutionId", nullable = false)
     private Institution institution;
 }
