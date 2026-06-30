24c38fd41e722a7811ce8803eb517ee8c3e1742a
Benney-Walker <benjaminbadubenney@gmail.com>
2026-06-30 08:59:33 +0000

    Cleared boiler plate

--- src/main/java/com/codewithben/schoolmanagementsystem/Entity/Level.java
+++ src/main/java/com/codewithben/schoolmanagementsystem/Entity/Level.java
@@ -1,9 +1,14 @@
 package com.codewithben.schoolmanagementsystem.Entity;
 
 import jakarta.persistence.*;
+import lombok.*;
 
 import java.util.List;
-
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 @Entity
 public class Level {
 
@@ -28,68 +33,4 @@ public class Level {
 
     @OneToMany(mappedBy = "level")
     private List<Fees> fees;
-
-    public String getLevelID() {
-
-        return levelID;
-    }
-
-    public void setLevelID(String classId) {
-        this.levelID = classId;
-    }
-
-    public String getLevelName() {
-
-        return levelName;
-    }
-
-    public void setLevelName(String className) {
-
-        this.levelName = className;
-    }
-
-    public Staffs getStaff() {
-
-        return staff;
-    }
-
-    public void setStaff(Staffs instructorId) {
-
-        this.staff = instructorId;
-    }
-
-    public Institution getInstitution() {
-        return institution;
-    }
-
-    public void setInstitution(Institution institution) {
-        this.institution = institution;
-    }
-
-    public List<Subjects> getSubjects() {
-        return subjects;
-    }
-
-    public void setSubjects(List<Subjects> subjectId) {
-
-        this.subjects = subjectId;
-    }
-
-    public List<Students> getStudents() {
-
-        return students;
-    }
-
-    public void setStudents(List<Students> students) {
-
-        this.students = students;
-    }
-
-    public List<Fees> getFees() {
-        return fees;
-    }
-
-    public void setFees(List<Fees> fees) {
-        this.fees = fees;
-    }
 }
